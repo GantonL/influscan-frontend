2da9e392fb78fd75a74f24c503a3486bef53548e
GantonL <45093712+GantonL@users.noreply.github.com>
2024-10-21 08:42:18 +0300

    basic styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex items-center justify-center h-[75vh]">
+  <div class="flex flex-col items-center gap-2">
+    <h1 class="text-4xl font-bold">InfluScan</h1>
+    <p class="text-lg">Identify your customers influencial footprint</p>
+    <input type="file">
+  </div>
+</div>
