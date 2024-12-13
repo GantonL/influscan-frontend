6473e58e463217516fed19bd10a3f726e9c777ef
GantonL <45093712+GantonL@users.noreply.github.com>
2024-12-13 16:45:15 +0200

    cloudflare pages deploy

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -8,6 +8,6 @@ export const handle: Handle = sequence(
 		debug: true,
 		protectedPaths: ['/scans'],
 		signInUrl: '/sign-in',
-		authorizedParties: ['http://localhost:5173'],
+		authorizedParties: ['http://localhost:5173', 'influscan.pages.dev'],
 	})
 )
