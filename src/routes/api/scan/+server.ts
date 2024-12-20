5d44da25a347743c4266de783b578bbcc3c7e0bf
GantonL <45093712+GantonL@users.noreply.github.com>
2024-12-20 21:54:36 +0200

    rescan bug fix

--- src/routes/api/scan/+server.ts
+++ src/routes/api/scan/+server.ts
@@ -37,6 +37,39 @@ export const POST: RequestHandler = async ({request, locals, fetch}) => {
   return json(response);
 }
 
+export const PUT: RequestHandler = async ({request, locals, fetch}) => {
+  const data = await request.formData();
+  const userId = locals.session?.id;
+  const scanData = data.get('data')?.toString();
+  const response: {success: boolean, scanResult?: ScanResult} = { success: false };
+  if (!userId || !scanData) { error(400); }
+  const parsedScanData = JSON.parse(scanData);
+  const searchResults = await search(parsedScanData.details, { fetch });
+  if (searchResults.error) {
+    return handleFailedScan(parsedScanData, response, { fetch });
+  }
+  const analysisResult = await analyze(parsedScanData.details.name , searchResults, { fetch });
+  if (!analysisResult) {
+    return handleFailedScan(parsedScanData, response, { fetch });
+  }
+  parsedScanData.estimation = analysisResult.estimation; 
+  parsedScanData.explanation = analysisResult.explanation;
+  parsedScanData.status = 'completed';
+  const updateScanObjectRes = await updateScanObject(parsedScanData.id, { 
+    status: parsedScanData.status, 
+    estimation: parsedScanData.estimation, 
+    explanation: parsedScanData.explanation 
+  }, { fetch });
+  response.success = !!updateScanObjectRes;
+  if (!response.success) {
+    error(500, 'Failed to update completed scan object');
+  }
+  response.scanResult = parsedScanData;
+  return json(response);
+}
+
+
+
 const handleFailedScan = async (scan: ScanResult, response: {success: boolean, scanResult?: ScanResult}, options?: { fetch?: RequestEvent['fetch'] }) => {
   scan.status = 'failed';
   const updateScanObjectRes = await updateScanObject(scan.id, { status: scan.status }, options);
