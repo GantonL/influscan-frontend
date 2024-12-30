6e65ae8eb8ea3c1d268e89314d424fca91dae77c
GantonL <45093712+GantonL@users.noreply.github.com>
2024-12-30 20:50:56 +0200

    avatars & plans wip

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,27 @@
 <script lang="ts">
+	import Avatar from "$lib/components/avatar/avatar.svelte";
 	import { Button } from "$lib/components/ui/button";
 	import { title } from "$lib/stores";
 	import SignedIn from "clerk-sveltekit/client/SignedIn.svelte";
 	import SignedOut from "clerk-sveltekit/client/SignedOut.svelte";
+	import FunnyAvatars from "./funny-avatars.svelte";
+	import Plans from "./plans.svelte";
   title.clear();
 </script>
-<div class="flex flex-col gap-6 align-start max-w-[400px]">
-  <h1 class="text-4xl text-balance">Discover your customers influencial footprint</h1>
-  <p class="text-base text-balance">Grow your business by increasing engagement with local influencenres</p>
-  <div class="flex flex-row gap-4">
-    <SignedIn>
-      <Button><a href="/scans">Get started</a></Button>
-    </SignedIn>
-    <SignedOut>
-      <Button variant="outline"><a href="/sign-in">Sign in</a></Button>
-      <Button><a href="/support/demo">Request a demo</a></Button>
-    </SignedOut>
+<div class="flex flex-row flex-wrap gap-8">
+  <div class="flex flex-col gap-6 align-start max-w-[400px]">
+    <h1 class="text-4xl text-balance">Discover your customers influencial footprint</h1>
+    <p class="text-base text-balance">Grow your business by increasing engagement with local influenceres</p>
+    <div class="flex flex-row gap-4">
+      <SignedIn>
+        <Button><a href="/scans">Get started</a></Button>
+      </SignedIn>
+      <SignedOut>
+        <Button variant="outline"><a href="/sign-in">Sign in</a></Button>
+        <Button>Try for free</Button>
+      </SignedOut>
+    </div>
   </div>
+  <FunnyAvatars />
 </div>
+<Plans />
---
+++ src/routes/plans.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+	import Button from "$lib/components/ui/button/button.svelte";
+  import * as Card from "$lib/components/ui/card";
+	import { Check } from "lucide-svelte";
+
+
+</script>
+<div class="flex flex-row flex-wrap items-start justify-center gap-4">
+  
+  <Card.Root>
+    <Card.Header class="flex flex-col items-center">
+      <Card.Title>Lite</Card.Title>
+    </Card.Header>
+    <Card.Content class="flex flex-col items-center">
+      <h3 class="text-xl text-bold">&dollar;5<span class="text-muted-foreground text-sm">/month</span></h3>
+      <ul class="flex flex-col gap-4 items-start p-2 w-full">
+        <li class="flex flex-row gap-2 items-center"><Check size=12/>Up to 250 scans per month</li>
+        <li class="flex flex-row gap-2 items-center"><Check size=12/>Maximum 20 scans per day</li>
+        <li class="flex flex-row gap-2 items-center"><Check size=12/>CSV upload</li>
+        <li class="flex flex-row gap-2 items-center"><Check size=12/>Strict CSV parser</li>
+      </ul>
+    </Card.Content>
+    <Card.Footer>
+      <Button class="w-full">Sounds good</Button>
+    </Card.Footer>
+  </Card.Root>
+  <Card.Root class="border-2 border-primary">
+    <Card.Header class="flex flex-col items-center">
+      <Card.Title>Pro</Card.Title>
+    </Card.Header>
+    <Card.Content class="flex flex-col items-center">
+      <h3 class="text-xl text-bold">&dollar;10<span class="text-muted-foreground text-sm">/month</span></h3>
+      <ul class="flex flex-col gap-4 items-start p-2 w-full">
+        <li class="flex flex-row gap-2 items-center"><Check size=12/>Up to 1000 scans per month</li>
+        <li class="flex flex-row gap-2 items-center"><Check size=12/>Maximum 100 scans per day</li>
+        <li class="flex flex-row gap-2 items-center"><Check size=12/>CSV upload</li>
+        <li class="flex flex-row gap-2 items-center"><Check size=12/>Dynamic CSV parser</li>
+        <li class="flex flex-row gap-2 items-center"><Check size=12/>API key</li>
+      </ul>
+    </Card.Content>
+    <Card.Footer>
+      <Button class="w-full">Oh yeah!</Button>
+    </Card.Footer>
+  </Card.Root>
+  <Card.Root>
+    <Card.Header class="flex flex-col items-center">
+      <Card.Title>Bro</Card.Title>
+    </Card.Header>
+    <Card.Content class="flex flex-col items-center">
+      <h3 class="text-xl text-bold">&dollar;50<span class="text-muted-foreground text-sm">/month</span></h3>
+      <ul class="flex flex-col gap-4 items-start p-2 w-full">
+        <li class="flex flex-row gap-2 items-center"><Check size=12/>Up to 10000 scans per month</li>
+        <li class="flex flex-row gap-2 items-center"><Check size=12/>Maximum 1000 scans per day</li>
+        <li class="flex flex-row gap-2 items-center"><Check size=12/>CSV upload</li>
+        <li class="flex flex-row gap-2 items-center"><Check size=12/>Dynamic CSV parser</li>
+        <li class="flex flex-row gap-2 items-center"><Check size=12/>API key</li>
+      </ul>
+    </Card.Content>
+    <Card.Footer>
+      <Button class="w-full">Damnnn</Button>
+    </Card.Footer>
+  </Card.Root>
+</div>
