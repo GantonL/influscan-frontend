4daf07c129716a204c3db1543764265ac83e3fbc
GantonL <45093712+GantonL@users.noreply.github.com>
2025-01-06 08:33:31 +0200

    animate plan card icon and remove redundant item

--- src/routes/plans.svelte
+++ src/routes/plans.svelte
@@ -7,16 +7,15 @@
 </script>
 <div class="flex flex-row flex-wrap items-start justify-center gap-4">
   
-  <Card.Root>
+  <Card.Root class="group">
     <Card.Header class="flex flex-col items-center">
-      <Card.Title class="flex flex-row gap-2 items-center"><Heart size=24/> Lite</Card.Title>
+      <Card.Title class="flex flex-row gap-2 items-center"><Heart size=24 class="group-hover:animate-ping"/> Lite</Card.Title>
       <Card.Description>For individuals</Card.Description>
     </Card.Header>
     <Card.Content class="flex flex-col items-center">
       <h3 class="text-3xl text-bold">&dollar;5<span class="text-muted-foreground text-sm">/month</span></h3>
       <ul class="flex flex-col gap-4 items-start p-2 w-full">
         <li class="flex flex-row gap-2 items-center"><Check size=12/>Up to 250 scans per month</li>
-        <li class="flex flex-row gap-2 items-center"><Check size=12/>Maximum 20 scans per day</li>
         <li class="flex flex-row gap-2 items-center"><Check size=12/>CSV upload</li>
         <li class="flex flex-row gap-2 items-center"><Check size=12/>Strict CSV parser</li>
         <li class="flex flex-row gap-2 items-center text-muted-foreground"><X size=12/>API key</li>
@@ -27,16 +26,15 @@
       <Button class="w-full">Let's go!</Button>
     </Card.Footer>
   </Card.Root>
-  <Card.Root class="border-2 border-primary">
+  <Card.Root class="group border-2 border-primary">
     <Card.Header class="flex flex-col items-center">
-      <Card.Title class="flex flex-row gap-2 items-center"><Spade size=24/> Plus</Card.Title>
+      <Card.Title class="flex flex-row gap-2 items-center"><Spade size=24 class="group-hover:animate-bounce"/> Plus</Card.Title>
       <Card.Description>For small businesses</Card.Description>
     </Card.Header>
     <Card.Content class="flex flex-col items-center">
       <h3 class="text-3xl text-bold">&dollar;25<span class="text-muted-foreground text-sm">/month</span></h3>
       <ul class="flex flex-col gap-4 items-start p-2 w-full">
         <li class="flex flex-row gap-2 items-center"><Check size=12/>Up to 1000 scans per month</li>
-        <li class="flex flex-row gap-2 items-center"><Check size=12/>Maximum 100 scans per day</li>
         <li class="flex flex-row gap-2 items-center"><Check size=12/>CSV upload</li>
         <li class="flex flex-row gap-2 items-center"><Check size=12/>Dynamic CSV parser</li>
         <li class="flex flex-row gap-2 items-center"><Check size=12/>API key</li>
@@ -47,16 +45,15 @@
       <Button class="w-full">Let's go!</Button>
     </Card.Footer>
   </Card.Root>
-  <Card.Root>
+  <Card.Root class="group">
     <Card.Header class="flex flex-col items-center">
-      <Card.Title class="flex flex-row gap-2 items-center"><Sparkle size=24/> Pro</Card.Title>
+      <Card.Title class="flex flex-row gap-2 items-center"><Sparkle size=24 class="group-hover:animate-spin"/> Pro</Card.Title>
       <Card.Description>For large companies</Card.Description>
     </Card.Header>
     <Card.Content class="flex flex-col items-center">
       <h3 class="text-3xl text-bold">&dollar;250<span class="text-muted-foreground text-sm">/month</span></h3>
       <ul class="flex flex-col gap-4 items-start p-2 w-full">
         <li class="flex flex-row gap-2 items-center"><Check size=12/>Up to 10000 scans per month</li>
-        <li class="flex flex-row gap-2 items-center"><Check size=12/>Maximum 1000 scans per day</li>
         <li class="flex flex-row gap-2 items-center"><Check size=12/>CSV upload</li>
         <li class="flex flex-row gap-2 items-center"><Check size=12/>Dynamic CSV parser</li>
         <li class="flex flex-row gap-2 items-center"><Check size=12/>API key</li>
