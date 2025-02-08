2cc51c623422a8338b9a046ad7834bb8562c1482
GantonL <45093712+GantonL@users.noreply.github.com>
2025-02-08 21:20:27 +0200

    truncate result explanation on smaller screens

--- src/lib/components/scan-result-explanaiton/scan-result-explanation.svelte
+++ src/lib/components/scan-result-explanaiton/scan-result-explanation.svelte
@@ -1,6 +1,14 @@
 <script lang="ts">
 	import type { ScanResult } from "$lib/models/scan";
+	import * as Tooltip from "../ui/tooltip";
 
   let { explanation }: {explanation: ScanResult['explanation']} = $props();
 </script>
-<span class="italic">{explanation}</span>
+<Tooltip.Provider>
+  <Tooltip.Root>
+    <Tooltip.Trigger>
+      <div class="text-start italic max-lg:w-96 max-lg:truncate">{explanation}</div>
+    </Tooltip.Trigger>
+    <Tooltip.Content>{explanation}</Tooltip.Content>
+  </Tooltip.Root>
+</Tooltip.Provider>
