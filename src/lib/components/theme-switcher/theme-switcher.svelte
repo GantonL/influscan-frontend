796fe796771c11534fa0404ff3e6655f2e3245c1
GantonL <45093712+GantonL@users.noreply.github.com>
2025-05-21 22:53:54 +0300

    Fied theme switcher icon placement issue

--- src/lib/components/theme-switcher/theme-switcher.svelte
+++ src/lib/components/theme-switcher/theme-switcher.svelte
@@ -5,10 +5,10 @@
 </script>
 <Button onclick={toggleMode} variant="outline" size="icon" class="aspect-square">
   <Sun
-    class="h-[1.2rem] w-[1.2rem] rotate-0 scale-0 transition-all dark:-rotate-90 dark:scale-100"
+    class="h-[1.2rem] w-[1.2rem] rotate-0 scale-0 transition-all dark:-rotate-90 dark:scale-100 hidden dark:block"
   />
   <Moon
-    class="absolute h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:rotate-90 dark:scale-0"
+    class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:rotate-90 dark:scale-0 dark:hidden block"
   />
   <span class="sr-only">Toggle theme</span>
 </Button>
